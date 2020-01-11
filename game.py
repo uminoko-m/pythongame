561ad8d97a0d4cfcef91fc07eb891c7807efb62c
uminoko <uminoko0105@gmail.com>
2020-01-11 10:55:33 +0900

    draw character

--- game.py
+++ game.py
@@ -1,13 +1,19 @@
 import pyxel
 
-pyxel.init(160, 120)
+class App:
+    def __init__(self):
+        pyxel.init(255, 180)        #window sizeの指定　最大(255,255)
+        pyxel.load("picture.pyxres")     #画像読み込み
+        pyxel.run(self.update, self.draw)
 
-def update():
-    if pyxel.btnp(pyxel.KEY_Q):
-        pyxel.quit()
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
 
-def draw():
-    pyxel.cls(0)
-    pyxel.rect(10, 10, 20, 20, 11)
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.blt(48, 45, 0, 0, 0, -48, 72,0)       #勇者
+        pyxel.blt(100, 45, 1, 0, 0, -55, 72,0)      #魔法使い
+        pyxel.blt(150, 45, 2, 0, 0, -48, 72,0)      #戦士
 
-pyxel.run(update, draw)
+App()
