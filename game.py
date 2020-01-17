f8af16971159fd967efa66382353a98c27d1d0cb
uminoko <uminoko0105@gmail.com>
2020-01-17 11:55:05 +0900

    fire recrection

--- game.py
+++ game.py
@@ -28,17 +28,21 @@ class App:
         
     def game_start(self):           #ゲーム開始
         self.direction = RIGHT
-
         # スコア
         self.score = 0
 
         # 始めの位置
-        self.player_x = 42
+        self.player_x = 20
         self.player_y = 60
         self.player_vy = 0
-        self.monster = [(randint(200,240),randint(30,150), True)for i in range(4)]
+        self.monster = [(200,randint(30,100), True)for i in range(1)]
+        self.fire=[]
+        self.flag=0
+        for i in range(len(self.monster)):
+            self.fire.append((self.monster[i][0]-16,self.monster[i][1],self.monster[i][2],self.flag))
+
         self.star=[(randint(0,255),randint(0,150), True)for i in range(25)]
- 
+
         pyxel.playm(0, loop=True)
         pyxel.run(self.update_game, self.draw_game)
 
@@ -48,7 +52,10 @@ class App:
 
         self.update_player()            #キャラ操作
 
-        for i, v in enumerate(self.monster):      #魔物出現
+        for i, v in enumerate(self.fire):    #炎
+            self.fire[i] = self.update_fire(*v)
+
+        for i, v in enumerate(self.monster):    #魔物
             self.monster[i] = self.update_monster(*v)
 
         for i, v in enumerate(self.star):      #星
@@ -56,14 +63,6 @@ class App:
 
 
     def update_player(self):        #キャラ操作詳細
-        if pyxel.btn(pyxel.KEY_LEFT):
-            self.player_x = max(self.player_x - 2, 0)
-            self.direction = LEFT
- 
-        if pyxel.btn(pyxel.KEY_RIGHT):
-            self.player_x = min(self.player_x + 2, pyxel.width - 16)
-            self.direction = RIGHT
-
         if pyxel.btn(pyxel.KEY_UP):
             self.player_y = max(self.player_y - 2, 0)
             self.direction = UP
@@ -80,12 +79,19 @@ class App:
             if is_active:
                 pyxel.blt(x, y, 0, 16, 104, 6, 6, 0)
  
-        # 魔物と火
+        # 火
+        for x, y, is_active,flag in self.fire:
+            if is_active:
+                if flag==0:
+                    pyxel.blt(x,y,0,16,88,16,16,0)
+                else:
+                    pyxel.blt(x,y,0,16,88,-16,16,0)
+
+        # 魔物
         for x, y, is_active in self.monster:
             if is_active:
-                pyxel.blt(x+16, y, 0, 0, 90, 16, 14, 0)
-                pyxel.blt(x,y,0,16,88,16,16,0)
- 
+                pyxel.blt(x, y, 0, 24, 72, 31, 15, 0)
+
         # 勇者ミニ
         pyxel.blt(
             self.player_x,
@@ -118,26 +124,52 @@ class App:
             is_active = True
         return (x, y, is_active)
 
-    def update_monster(self, x, y, is_active):
-        if is_active and abs(x - self.player_x) < 12 and abs(y - self.player_y) < 12:
-            if pyxel.btn(pyxel.KEY_SPACE):      #スペースキーを押したらスコア反映
-                is_active = False
-                self.score += 100
-                self.player_vy = min(self.player_vy, -8)
-                pyxel.play(3, 4)
-            else:
-                is_active = False
-                
+    def update_monster(self,x,y,is_active):
+        if is_active and abs(x - self.fire_x) < 10 and abs(y-self.fire_y)< 10:
+            is_active = False
+            self.score += 100
+            self.flag=0
 
-        n=2
-        x-=n
-
-        if x < -40:
-            x += 290
+        if is_active==False:
+            x =225
             y = randint(30, 150)
             is_active = True
- 
-        return (x, y, is_active)
+
+        self.monster_x=x
+        self.monster_y=y
+        return(x,y,is_active)
+
+    def update_fire(self, x, y, is_active,flag):
+        if is_active and abs(x - self.player_x) < 12 and abs(y - self.player_y) < 12:
+            if pyxel.btn(pyxel.KEY_SPACE):      #スペースキーを押したら炎反転
+                is_active = True
+                self.score += 100
+                flag=1
+                #self.player_vy = min(self.player_vy, -8)
+                #pyxel.play(3, 4)
+            else:
+                is_active=False
+
+        if flag==1:
+            x+=3
+        else:
+            x-=3
+
+        if x < -40:
+            x=self.monster_x-16
+            y=self.monster_y
+            is_active = True
+            flag=0
+
+        if x > 250:
+            x=self.monster_x-16
+            y=self.monster_y
+            is_active = True
+            flag=0
+
+        self.fire_x=x
+        self.fire_y=y
+        return (x, y, is_active,flag)
 
 
 App()
